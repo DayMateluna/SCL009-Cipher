fa524f7de9bffa7407a268379d2ba09f609caef8
DayMateluna <d.mateluna.c@gmail.com>
2019-04-09 13:03:21 -0400

    cifrando y decifrando npm

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -14,8 +14,15 @@ describe('cipher', () => {
       assert.equal(window.cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
     });
       
-    it('debería retornar "hijklmnopqrstuvwxyzaccdefghijkl" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () =>{
-      assert.equal(window.cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"), "abcdefghijklmnopqrstuvwxyz");   
+
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () =>{
+      assert.equal(window.cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");   
+      it('debería retornar "345678901" para "123456789" con offset 2', () =>{
+        assert.equal(window.cipher.decode(2,"123456789"), "345678901")
+      });
+      it('debería retornar "+*" para "$#" con offset 7', () =>{
+        assert.equal(window.cipher.decode(7,"$#"),"+*")
+        });
     });
   });
 
@@ -28,9 +35,14 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () =>{
     assert.equal(window.cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     });
-    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzaccdefghijkl" con offset 33', () =>{
-      assert.equal(window.cipher.decode(33,"abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzaccdefghijkl");
+    it('debería retornar "abcdefg" para "klmnopq" con offset 10', () =>{
+      assert.equal(window.cipher.decode(10,"klmnopq"),"abcdefg");
     });
-  
+    it('debería retornar "123456" para "234567" con offset 1', () =>{
+      assert.equal(window.cipher.decode(1,"234567"), "123456")
+    });
+    it('debería retornar "+.$(!"%," para "#&+/(),$" con offset 8', () =>{
+      assert.equal(window.cipher.decode(8,""), "")
+      });
 })
 });
